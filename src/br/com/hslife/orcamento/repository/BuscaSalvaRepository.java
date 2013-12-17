fc1be1acf2ef7382f041af4e1f44c3137c5f86e7
herculeshssj <herculeshssj@790cd0c2-49b5-4a51-aabf-19842a6fa872>
2013-12-17 12:03:36 +0000

    Corrigido problema de mostrar buscas salvas de lançamentos do cartão a listagem de buscas salvas de lançamentos da conta. refs #879 @15m
    git-svn-id: svn://hslife.com.br/hslife/orcamento/trunk@850 790cd0c2-49b5-4a51-aabf-19842a6fa872

--- src/br/com/hslife/orcamento/repository/BuscaSalvaRepository.java
+++ src/br/com/hslife/orcamento/repository/BuscaSalvaRepository.java
@@ -72,28 +72,28 @@ public class BuscaSalvaRepository extends AbstractCRUDRepository<BuscaSalva> {
 	
 	@SuppressWarnings("unchecked")
 	public List<BuscaSalva> findAllByUsuario(Usuario usuario) {
-		String sql = "select b.* from buscasalva b inner join conta c on c.id = b.idConta where c.idUsuario = :idUsuario order by b.descricao asc";
+		String sql = "select b.* from buscasalva b inner join conta c on c.id = b.idConta where c.idUsuario = :idUsuario and not c.tipoConta = 'CARTAO' order by b.descricao asc";
 		Query query = getSession().createSQLQuery(sql).addEntity(BuscaSalva.class).setLong("idUsuario", usuario.getId());
 		return query.list();
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<BuscaSalva> findAllEnabledContaByUsuario(Usuario usuario) {
-		String sql = "select b.* from buscasalva b inner join conta c on c.id = b.idConta where c.idUsuario = :idUsuario and c.ativo = true order by b.descricao asc";
+		String sql = "select b.* from buscasalva b inner join conta c on c.id = b.idConta where c.idUsuario = :idUsuario and c.ativo = true and not c.tipoConta = 'CARTAO' order by b.descricao asc";
 		Query query = getSession().createSQLQuery(sql).addEntity(BuscaSalva.class).setLong("idUsuario", usuario.getId());
 		return query.list();
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<BuscaSalva> findAllByContaAndUsuario(Conta conta, Usuario usuario) {
-		String sql = "select b.* from buscasalva b inner join conta c on c.id = b.idConta and c.id = :idConta where c.idUsuario = :idUsuario order by b.descricao asc";
+		String sql = "select b.* from buscasalva b inner join conta c on c.id = b.idConta and c.id = :idConta where c.idUsuario = :idUsuario and not c.tipoConta = 'CARTAO' order by b.descricao asc";
 		Query query = getSession().createSQLQuery(sql).addEntity(BuscaSalva.class).setLong("idUsuario", usuario.getId()).setLong("idConta", conta.getId());
 		return query.list();
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<BuscaSalva> findAllEnabledContaByContaAndUsuario(Conta conta, Usuario usuario) {
-		String sql = "select b.* from buscasalva b inner join conta c on c.id = b.idConta and c.id = :idConta where c.idUsuario = :idUsuario and c.ativo = true order by b.descricao asc";
+		String sql = "select b.* from buscasalva b inner join conta c on c.id = b.idConta and c.id = :idConta where c.idUsuario = :idUsuario and c.ativo = true and not c.tipoConta = 'CARTAO' order by b.descricao asc";
 		Query query = getSession().createSQLQuery(sql).addEntity(BuscaSalva.class).setLong("idUsuario", usuario.getId()).setLong("idConta", conta.getId());
 		return query.list();
 	}
