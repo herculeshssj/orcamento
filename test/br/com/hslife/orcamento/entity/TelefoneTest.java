8b6818992fd068e19c7960b342e8bc351b311aec
herculeshssj <herculeshssj@790cd0c2-49b5-4a51-aabf-19842a6fa872>
2014-01-19 19:18:08 +0000

    Elaboração dos testes para o cadastro de telefones do usuário. refs #817 @30m
    git-svn-id: svn://hslife.com.br/hslife/orcamento/trunk@898 790cd0c2-49b5-4a51-aabf-19842a6fa872

---
+++ test/br/com/hslife/orcamento/entity/TelefoneTest.java
@@ -0,0 +1,152 @@
+/***
+  
+  	Copyright (c) 2012, 2013, 2014 Hércules S. S. José
+
+    Este arquivo é parte do programa Orçamento Doméstico.
+    
+
+    Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou 
+
+    modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como 
+
+    publicada pela Fundação do Software Livre (FSF); na versão 2.1 da 
+
+    Licença.
+    
+
+    Este programa é distribuído na esperança que possa ser útil, 
+
+    mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÂO a 
+    
+    qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública 
+    
+    Geral Menor GNU em português para maiores detalhes.
+    
+
+    Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob o 
+
+    nome de "LICENSE.TXT" junto com este programa, se não, acesse o site HSlife
+    
+    no endereco www.hslife.com.br ou escreva para a Fundação do Software 
+    
+    Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
+    
+
+    Para mais informações sobre o programa Orçamento Doméstico e seu autor acesse o 
+
+    endereço www.hslife.com.br, pelo e-mail contato@hslife.com.br ou escreva para 
+
+    Hércules S. S. José, Av. Ministro Lafaeyte de Andrade, 1683 - Bl. 3 Apt 404, 
+
+    Marco II - Nova Iguaçu, RJ, Brasil.
+  
+ ***/
+
+package br.com.hslife.orcamento.entity;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.hslife.orcamento.exception.BusinessException;
+
+public class TelefoneTest {
+	
+	private Telefone entity;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome("Usuário de teste");
+		
+		entity = new Telefone();
+		entity.setDdd("21");
+		entity.setDescricao("Comercial");
+		entity.setNumero("32936010");
+		entity.setRamal("6010");
+		entity.setUsuario(usuario);
+		
+	}
+
+	@Test
+	public void testGetLabel() {
+		assertEquals("Comercial: (21) 32936010, Ramal: 6010", entity.getLabel());
+	}
+	
+	@Test
+	public void testValidateDescricao() {
+		try {
+			entity.setDescricao("     ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ      ");
+			entity.validate();
+		} catch (BusinessException be) {
+			assertEquals("Campo 'Descrição' aceita no máximo 50 caracteres!", be.getMessage());
+			return;
+		} catch (Throwable t) {
+			fail(t.getMessage());
+		}
+		fail("Falha no teste!");
+	}
+
+	@Test	
+	public void testValidateDDD() {
+		try {
+			entity.setDdd(null);
+			entity.validate();
+			entity.setDdd("     ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ      ");
+			entity.validate();
+		} catch (BusinessException be) {
+			assertEquals("Campo 'DDD' aceita no máximo 5 caracteres!", be.getMessage());
+			return;
+		} catch (Throwable t) {
+			fail(t.getMessage());
+		}
+		fail("Falha no teste!");
+	}
+	
+	@Test
+	public void testValidateNumero() {
+		try {			
+			entity.setNumero("     ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ      ");
+			entity.validate();
+		} catch (BusinessException be) {
+			assertEquals("Campo 'Número' aceita no máximo 15 caracteres!", be.getMessage());
+			return;
+		} catch (Throwable t) {
+			fail(t.getMessage());
+		}
+		fail("Falha no teste!");
+	}
+	
+	@Test
+	public void testValidateRamal() {
+		try {
+			entity.setRamal(null);
+			entity.validate();
+			entity.setRamal("     ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ      ");
+			entity.validate();
+		} catch (BusinessException be) {
+			assertEquals("Campo 'Ramal' aceita no máximo 5 caracteres!", be.getMessage());
+			return;
+		} catch (Throwable t) {
+			fail(t.getMessage());
+		}
+		fail("Falha no teste!");
+	}
+	
+	@Test
+	public void testValidateUsuario() {
+		try {
+			entity.setUsuario(null);
+			entity.validate();
+		} catch (BusinessException be) {
+			assertEquals("Informe o usuário!", be.getMessage());
+			return;
+		} catch (Throwable t) {
+			fail(t.getMessage());
+		}
+		fail("Falha no teste!");
+	}
+}
