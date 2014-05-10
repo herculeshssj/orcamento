be9566de2701c028d66ff5b8610b4fb52e5594f5
herculeshssj <herculeshssj@790cd0c2-49b5-4a51-aabf-19842a6fa872>
2014-05-10 19:44:27 +0000

    Atualizado os scripts da base. refs #1011
    git-svn-id: svn://hslife.com.br/hslife/orcamento/branches/MAI2014-hotfixes@1049 790cd0c2-49b5-4a51-aabf-19842a6fa872

--- docs/script-update-version-db.sql
+++ docs/script-update-version-db.sql
@@ -44,13 +44,8 @@
 
 /*** Script de atualização da base de dados ***/
 
-/*** ATUALIZAÇÃO DA BASE DE DADOS PARA A VERSÃO MAI2014.1 ***/
+/*** ATUALIZAÇÃO DA BASE DE DADOS PARA A VERSÃO MAI2014.2 ***/
 
 -- Atualização de versão
 update versao set ativo = false;
-insert into versao (versao, ativo) values ('MAI2014.1', true);
-
--- Exclusão dos dados sensíveis do cartão de crédito
--- Tarefa #1003
-update cartaocredito set codigoSeguranca = null, nomeCliente = null, numeroCartao = null;
-delete from auditoria where classe = 'CartaoCredito';
+insert into versao (versao, ativo) values ('MAI2014.2', true);
