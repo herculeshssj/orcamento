2cee07245e1b1741880377e87e6628783c593de3
herculeshssj <herculeshssj@hslife.com.br>
2014-07-04 13:48:11 -0300

    Atualizado a versão no arquivo de atualização da base. refs #973

--- docs/script-update-version-db.sql
+++ docs/script-update-version-db.sql
@@ -44,11 +44,11 @@
 
 /*** Script de atualização da base de dados ***/
 
-/*** ATUALIZAÇÃO DA BASE DE DADOS PARA A VERSÃO AGO2014 ***/
+/*** ATUALIZAÇÃO DA BASE DE DADOS PARA A VERSÃO JUL2014.1 ***/
 
 -- Atualização de versão
 update versao set ativo = false;
-insert into versao (versao, ativo) values ('AGO2014', true);
+insert into versao (versao, ativo) values ('JUL2014.1', true);
 
 -- Inclusão da coluna codigoMonetario na tabela moeda
 -- Tarefa #1067
