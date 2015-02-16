50194ecda7e2fc3df19a42d7bc1912defa66bd93
Hércules S. S. José <hercules_hssj@outlook.com>
2015-02-16 15:39:12 -0200

    Simulação de um bug no DB. Issue #46

--- docs/script-update-production-db.sql
+++ docs/script-update-production-db.sql
@@ -92,7 +92,7 @@ begin
     /*** Entre com as atualizações da base aqui ***/
     
     update versao set ativo = false;
-	insert into versao (versao, ativo) values ('MAR2015', true);
+	insert into versao (id, versao, ativo) values (1, 'MAR2015', true);
 
     /*** Fim do bloco de atualizações da base ***/
     
