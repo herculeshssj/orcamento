a12d96b75755c8f9ac89ff1f3e817ce57ccf10d7
Hércules S. S. José <hercules_hssj@outlook.com>
2017-01-05 18:36:51 -0200

    Criado as classes de domínio, atualiza a documentação e os scripts SQL. Issue #239

---
+++ src/br/com/hslife/orcamento/entity/Investimento.java
@@ -0,0 +1,202 @@
+/***
+  
+  	Copyright (c) 2012 - 2020 Hércules S. S. José
+
+    Este arquivo é parte do programa Orçamento Doméstico.
+    
+
+    Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou 
+
+    modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como 
+
+    publicada pela Fundação do Software Livre (FSF); na versão 2.1 da 
+
+    Licença.
+    
+
+    Este programa é distribuído na esperança que possa ser útil, 
+
+    mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÂO a 
+    
+    qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública 
+    
+    Geral Menor GNU em português para maiores detalhes.
+    
+
+    Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob o 
+
+    nome de "LICENSE.TXT" junto com este programa, se não, acesse o site do
+    
+    projeto no endereco https://github.com/herculeshssj/orcamento ou escreva 
+    
+    para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth Floor, 
+    
+    Boston, MA  02110-1301, USA.
+    
+
+    Para mais informações sobre o programa Orçamento Doméstico e seu autor entre  
+
+    em contato pelo e-mail herculeshssj@outlook.com, ou ainda escreva para 
+
+    Hércules S. S. José, Av. Ministro Lafaeyte de Andrade, 1683 - Bl. 3 Apt 404, 
+
+    Marco II - Nova Iguaçu, RJ, Brasil.
+  
+***/
+
+package br.com.hslife.orcamento.entity;
+
+import java.util.SortedSet;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Sort;
+import org.hibernate.annotations.SortType;
+
+import br.com.caelum.stella.validation.CNPJValidator;
+import br.com.caelum.stella.validation.InvalidStateException;
+import br.com.hslife.orcamento.enumeration.TipoInvestimento;
+import br.com.hslife.orcamento.exception.ValidationException;
+import br.com.hslife.orcamento.util.EntityPersistenceUtil;
+import br.com.hslife.orcamento.util.MovimentacaoInvestimentoComparator;
+import br.com.hslife.orcamento.util.ResumoInvestimentoComparator;
+
+@Entity
+@Table(name="investimento")
+@SuppressWarnings("serial")
+public class Investimento extends EntityPersistence {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name="idBanco", nullable=false)
+	private Banco banco;
+	
+	@Column(length=10, nullable=false)
+	@Enumerated(EnumType.STRING)
+	private TipoInvestimento tipoInvestimento;
+	
+	@Column(length=50, nullable=false)
+	private String descricao;
+	
+	@Column(length=14, nullable=false)
+	private String cnpj;
+	
+	@OneToMany(fetch=FetchType.EAGER, orphanRemoval=true, cascade=CascadeType.ALL)	
+	@Sort(type=SortType.COMPARATOR, comparator=ResumoInvestimentoComparator.class)
+	private SortedSet<ResumoInvestimento> resumosInvestimento;
+	
+	@OneToMany(fetch=FetchType.EAGER, orphanRemoval=true, cascade=CascadeType.ALL)	
+	@Sort(type=SortType.COMPARATOR, comparator=MovimentacaoInvestimentoComparator.class)
+	private SortedSet<MovimentacaoInvestimento> movimentacoesInvestimento;
+	
+	@ManyToOne
+	@JoinColumn(name="idUsuario", nullable=false)
+	private Usuario usuario;
+	
+	public Investimento() {
+		
+	}
+	
+	@Override
+	public String getLabel() {
+		return this.descricao;
+	}
+
+	@Override
+	public void validate() {
+		EntityPersistenceUtil.validaCampoNulo("Banco", this.banco);
+		EntityPersistenceUtil.validaCampoNulo("Tipo de investimento", this.tipoInvestimento);
+		EntityPersistenceUtil.validaTamanhoCampoStringObrigatorio("Descrição", this.descricao, 50);
+		EntityPersistenceUtil.validaTamanhoExatoCampoStringObrigatorio("CNPJ", this.cnpj, 14);
+		
+		try {
+			CNPJValidator validatorCnpj = new CNPJValidator();
+			
+			if (this.getCnpj() != null && !this.getCnpj().trim().isEmpty()) {
+				validatorCnpj.assertValid(this.getCnpj());				
+			}			
+			
+		} catch (InvalidStateException ise) {
+			throw new ValidationException(ise);
+		}
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Banco getBanco() {
+		return banco;
+	}
+
+	public void setBanco(Banco banco) {
+		this.banco = banco;
+	}
+
+	public TipoInvestimento getTipoInvestimento() {
+		return tipoInvestimento;
+	}
+
+	public void setTipoInvestimento(TipoInvestimento tipoInvestimento) {
+		this.tipoInvestimento = tipoInvestimento;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public SortedSet<ResumoInvestimento> getResumosInvestimento() {
+		return resumosInvestimento;
+	}
+
+	public void setResumosInvestimento(SortedSet<ResumoInvestimento> resumosInvestimento) {
+		this.resumosInvestimento = resumosInvestimento;
+	}
+
+	public SortedSet<MovimentacaoInvestimento> getMovimentacoesInvestimento() {
+		return movimentacoesInvestimento;
+	}
+
+	public void setMovimentacoesInvestimento(SortedSet<MovimentacaoInvestimento> movimentacoesInvestimento) {
+		this.movimentacoesInvestimento = movimentacoesInvestimento;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
---
+++ src/br/com/hslife/orcamento/entity/MovimentacaoInvestimento.java
@@ -0,0 +1,219 @@
+/***
+  
+  	Copyright (c) 2012 - 2020 Hércules S. S. José
+
+    Este arquivo é parte do programa Orçamento Doméstico.
+    
+
+    Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou 
+
+    modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como 
+
+    publicada pela Fundação do Software Livre (FSF); na versão 2.1 da 
+
+    Licença.
+    
+
+    Este programa é distribuído na esperança que possa ser útil, 
+
+    mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÂO a 
+    
+    qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública 
+    
+    Geral Menor GNU em português para maiores detalhes.
+    
+
+    Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob o 
+
+    nome de "LICENSE.TXT" junto com este programa, se não, acesse o site do
+    
+    projeto no endereco https://github.com/herculeshssj/orcamento ou escreva 
+    
+    para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth Floor, 
+    
+    Boston, MA  02110-1301, USA.
+    
+
+    Para mais informações sobre o programa Orçamento Doméstico e seu autor entre  
+
+    em contato pelo e-mail herculeshssj@outlook.com, ou ainda escreva para 
+
+    Hércules S. S. José, Av. Ministro Lafaeyte de Andrade, 1683 - Bl. 3 Apt 404, 
+
+    Marco II - Nova Iguaçu, RJ, Brasil.
+  
+***/
+
+package br.com.hslife.orcamento.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import br.com.hslife.orcamento.util.EntityPersistenceUtil;
+
+@Entity
+@Table(name="investimento")
+@SuppressWarnings("serial")
+public class MovimentacaoInvestimento extends EntityPersistence {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length=10, nullable=false)
+	@Enumerated(EnumType.STRING)
+	private String tipoLancamento;
+	
+	@Column(nullable=true)
+	@Temporal(TemporalType.DATE)
+	private Date data;
+	
+	@Column(length=50, nullable=false)
+	private String historico;
+	
+	@Column(length=50, nullable=true)
+	private String documento;
+	
+	@Column(nullable=false, precision=18, scale=2)
+	private double valor;
+	
+	@Column(nullable=false, precision=18, scale=2)
+	private double impostoRenda;
+	
+	@Column(nullable=false, precision=18, scale=2)
+	private double iof;
+	
+	@Column(nullable=false, precision=18, scale=2)
+	private double compensacaoIR;
+	
+	@Column(nullable=false, precision=18, scale=6)
+	private double cotas;
+	
+	@Column(nullable=false, precision=18, scale=9)
+	private double valorCota;
+	
+	@Column(nullable=false, precision=18, scale=9)
+	private double saldoCotas;
+	
+	public MovimentacaoInvestimento() {
+		
+	}
+
+	@Override
+	public String getLabel() {
+		return this.historico;
+	}
+
+	@Override
+	public void validate() {
+		EntityPersistenceUtil.validaTamanhoCampoStringObrigatorio("Histórico", this.historico, 50);
+		EntityPersistenceUtil.validaCampoNulo("Tipo de lançamento", this.tipoLancamento);
+		EntityPersistenceUtil.validaCampoNulo("Data", this.data);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipoLancamento() {
+		return tipoLancamento;
+	}
+
+	public void setTipoLancamento(String tipoLancamento) {
+		this.tipoLancamento = tipoLancamento;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getHistorico() {
+		return historico;
+	}
+
+	public void setHistorico(String historico) {
+		this.historico = historico;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public double getImpostoRenda() {
+		return impostoRenda;
+	}
+
+	public void setImpostoRenda(double impostoRenda) {
+		this.impostoRenda = impostoRenda;
+	}
+
+	public double getIof() {
+		return iof;
+	}
+
+	public void setIof(double iof) {
+		this.iof = iof;
+	}
+
+	public double getCompensacaoIR() {
+		return compensacaoIR;
+	}
+
+	public void setCompensacaoIR(double compensacaoIR) {
+		this.compensacaoIR = compensacaoIR;
+	}
+
+	public double getCotas() {
+		return cotas;
+	}
+
+	public void setCotas(double cotas) {
+		this.cotas = cotas;
+	}
+
+	public double getValorCota() {
+		return valorCota;
+	}
+
+	public void setValorCota(double valorCota) {
+		this.valorCota = valorCota;
+	}
+
+	public double getSaldoCotas() {
+		return saldoCotas;
+	}
+
+	public void setSaldoCotas(double saldoCotas) {
+		this.saldoCotas = saldoCotas;
+	}
+}
