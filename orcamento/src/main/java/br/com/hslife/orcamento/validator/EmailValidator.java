fe15ca23c5afdd6f8499439f56cafe191642d668
Hércules S. S. José <hercules_hssj@outlook.com>
2017-06-16 23:09:51 -0300

    Atualizado o texto de licença de todas as classes utilitárias.

--- orcamento/src/main/java/br/com/hslife/orcamento/validator/EmailValidator.java
+++ orcamento/src/main/java/br/com/hslife/orcamento/validator/EmailValidator.java
@@ -1,47 +1,47 @@
 /***
   
-  	Copyright (c) 2012 - 2020 Hércules S. S. José
+  	Copyright (c) 2012 - 2021 Hércules S. S. José
 
     Este arquivo é parte do programa Orçamento Doméstico.
     
 
-    Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou 
+    Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou
 
-    modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como 
+    modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como
 
-    publicada pela Fundação do Software Livre (FSF); na versão 2.1 da 
+    publicada pela Fundação do Software Livre (FSF); na versão 3.0 da
 
     Licença.
     
 
-    Este programa é distribuído na esperança que possa ser útil, 
+    Este programa é distribuído na esperança que possa ser útil, mas SEM
 
-    mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÂO a 
+    NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÂO a qualquer
     
-    qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública 
+    MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral Menor GNU
     
-    Geral Menor GNU em português para maiores detalhes.
-    
-
-    Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob o 
-
-    nome de "LICENSE.TXT" junto com este programa, se não, acesse o site do
-    
-    projeto no endereco https://github.com/herculeshssj/orcamento ou escreva 
-    
-    para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth Floor, 
-    
-    Boston, MA  02110-1301, USA.
+    em português para maiores detalhes.
     
 
-    Para mais informações sobre o programa Orçamento Doméstico e seu autor entre  
+    Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob
+	
+	o nome de "LICENSE" junto com este programa, se não, acesse o site do
+    
+    projeto no endereco https://github.com/herculeshssj/orcamento-maven ou 
+	
+	escreva para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth 
+	
+	Floor, Boston, MA  02110-1301, USA.
+    
 
-    em contato pelo e-mail herculeshssj@outlook.com, ou ainda escreva para 
+    Para mais informações sobre o programa Orçamento Doméstico e seu autor
+	
+	entre em contato pelo e-mail herculeshssj@outlook.com, ou ainda escreva
 
-    Hércules S. S. José, Av. Ministro Lafaeyte de Andrade, 1683 - Bl. 3 Apt 404, 
+    para Hércules S. S. José, Rua José dos Anjos, 160 - Bl. 3 Apto. 304 - 
+	
+	Jardim Alvorada - CEP: 26261-130 - Nova Iguaçu, RJ, Brasil.
 
-    Marco II - Nova Iguaçu, RJ, Brasil.
-  
 ***/
 
 package br.com.hslife.orcamento.validator;
