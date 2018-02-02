90cf6bc98dcaffb082ba4a1c17f54371946066b0
Hércules S. S. José <hercules_hssj@outlook.com>
2018-02-02 21:16:06 -0200

    Resolvido o problema de lentidão ao iniciar o sistema.

--- orcamento/Scripts/migracaoSpring4Hibernate5.sql
+++ orcamento/Scripts/migracaoSpring4Hibernate5.sql
@@ -12,9 +12,6 @@ SELECT * FROM usuario;
 
 /***** SQL definitivo da atualização das bibliotecas *****/
 
--- Aumentado para 40 o número de caracteres do ID da sessão para trabalhar com Wildfly
-alter table logrequisicao change column `sessaoID` `sessaoID` char(40) null;
-
 -- Inclusão de browser e IP nos registros de auditoria
 alter table auditoria add column browser varchar(255) not null;
 alter table auditoria add column ip varchar(255) not null;
