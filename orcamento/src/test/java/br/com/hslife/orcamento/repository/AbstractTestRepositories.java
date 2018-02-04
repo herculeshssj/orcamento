3a0be4a67bb4fa6c09deea2fbc92e87bff2c64db
Hércules S. S. José <hercules_hssj@outlook.com>
2018-02-04 16:28:45 -0200

    Resolvido os warnings nas classes de testes.

--- orcamento/src/test/java/br/com/hslife/orcamento/repository/AbstractTestRepositories.java
+++ orcamento/src/test/java/br/com/hslife/orcamento/repository/AbstractTestRepositories.java
@@ -50,12 +50,10 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/spring-config-junit.xml"})
-@TransactionConfiguration
 @Transactional
 public abstract class AbstractTestRepositories extends AbstractTransactionalJUnit4SpringContextTests {
 
