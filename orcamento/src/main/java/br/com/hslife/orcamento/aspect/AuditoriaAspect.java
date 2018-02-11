2576b85323ab3ef705b117168c533649b15a1fe1
Hércules S. S. José <hercules_hssj@outlook.com>
2018-02-11 21:58:55 -0200

    Tentando melhorar a qualidade do código.

--- orcamento/src/main/java/br/com/hslife/orcamento/aspect/AuditoriaAspect.java
+++ orcamento/src/main/java/br/com/hslife/orcamento/aspect/AuditoriaAspect.java
@@ -1,44 +1,44 @@
 /***
-  
-  	Copyright (c) 2012 - 2021 Hércules S. S. José
 
-    Este arquivo é parte do programa Orçamento Doméstico.
-    
-
-    Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou
-
-    modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como
-
-    publicada pela Fundação do Software Livre (FSF); na versão 3.0 da
-
-    Licença.
-    
-
-    Este programa é distribuído na esperança que possa ser útil, mas SEM
-
-    NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer
-    
-    MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral Menor GNU
-    
-    em português para maiores detalhes.
-    
-
-    Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob
+	Copyright (c) 2012 - 2021 Hércules S. S. José
+	
+	Este arquivo é parte do programa Orçamento Doméstico.
+	
+	
+	Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou
+	
+	modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como
+	
+	publicada pela Fundação do Software Livre (FSF); na versão 3.0 da 
+	
+	Licença.
+	
+	
+	Este programa é distribuído na esperança que possa ser útil, mas SEM
+	
+	NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer
+	
+	MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral Menor 
+	
+	GNU em português para maiores detalhes.
+	
+	
+	Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob
 	
 	o nome de "LICENSE" junto com este programa, se não, acesse o site do
-    
-    projeto no endereco https://github.com/herculeshssj/orcamento-maven ou 
 	
-	escreva para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth 
+	projeto no endereco https://github.com/herculeshssj/orcamento ou escreva
 	
-	Floor, Boston, MA  02110-1301, USA.
-    
-
-    Para mais informações sobre o programa Orçamento Doméstico e seu autor
+	para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth Floor,
+	
+	Boston, MA  02110-1301, USA.
+	
+	
+	Para mais informações sobre o programa Orçamento Doméstico e seu autor
 	
 	entre em contato pelo e-mail herculeshssj@outlook.com, ou ainda escreva
-
-    para Hércules S. S. José, Rua José dos Anjos, 160 - Bl. 3 Apto. 304 - 
+	
+	para Hércules S. S. José, Rua José dos Anjos, 160 - Bl. 3 Apto. 304 -
 	
 	Jardim Alvorada - CEP: 26261-130 - Nova Iguaçu, RJ, Brasil.
 
@@ -73,17 +73,23 @@ public class AuditoriaAspect {
 	
 	@Autowired
 	private IAuditoria auditoriaService;
-	
-	public UsuarioComponent getUsuarioComponent() {
+
+	/**
+	 * @return the usuarioComponent
+	 */
+	public final UsuarioComponent getUsuarioComponent() {
 		return usuarioComponent;
 	}
 
-	public IAuditoria getAuditoriaService() {
+	/**
+	 * @return the auditoriaService
+	 */
+	public final IAuditoria getAuditoriaService() {
 		return auditoriaService;
 	}
 
 	@AfterReturning(pointcut="execution(public void br.com.hslife.orcamento.repository..save(..)) && args(entity)")
-	public void afterSave(EntityPersistence entity) {
+	public void afterSave(final EntityPersistence entity) {
 		try {
 			Auditoria auditoria = new Auditoria();
 			auditoria.setTransacao("INSERT");
@@ -110,7 +116,7 @@ public class AuditoriaAspect {
 			if (usuario == null) {
 				// Não existe sessão para o usuário, então registra-se o IP de onde o usuário está acessando
 				auditoria.setUsuario(auditoria.getIp());
-			} else {
+			} else if (usuario.getLogin() != null) {
 				auditoria.setUsuario(usuario.getLogin());
 			}
 			
@@ -123,7 +129,7 @@ public class AuditoriaAspect {
 	}
 	
 	@AfterReturning("execution(public void br.com.hslife.orcamento.repository..update(..)) && args(entity)")
-	public void afterUpdate(EntityPersistence entity) {
+	public void afterUpdate(final EntityPersistence entity) {
 		try {
 			Auditoria auditoria = new Auditoria();
 			auditoria.setTransacao("UPDATE");
@@ -150,7 +156,7 @@ public class AuditoriaAspect {
 			if (usuario == null) {
 				// Não existe sessão para o usuário, então registra-se o IP de onde o usuário está acessando
 				auditoria.setUsuario(auditoria.getIp());
-			} else {
+			} else if (usuario.getLogin() != null) {
 				auditoria.setUsuario(usuario.getLogin());
 			}
 			
@@ -163,7 +169,7 @@ public class AuditoriaAspect {
 	}
 	
 	@AfterReturning(pointcut="execution(public void br.com.hslife.orcamento.repository..delete(..)) && args(entity)")
-	public void beforeDelete(EntityPersistence entity) {
+	public void beforeDelete(final EntityPersistence entity) {
 		try {
 			Auditoria auditoria = new Auditoria();
 			auditoria.setTransacao("DELETE");
@@ -190,7 +196,7 @@ public class AuditoriaAspect {
 			if (usuario == null) {
 				// Não existe sessão para o usuário, então registra-se o IP de onde o usuário está acessando
 				auditoria.setUsuario(auditoria.getIp());
-			} else {
+			} else if (usuario.getLogin() != null) {
 				auditoria.setUsuario(usuario.getLogin());
 			}
 			
