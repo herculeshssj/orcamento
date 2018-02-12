56bb67b9682057690599c995422f66bd311767a2
Hércules S. S. José <hercules_hssj@outlook.com>
2018-02-12 21:56:02 -0200

    Otimização do código.

--- orcamento/src/main/java/br/com/hslife/orcamento/facade/IConta.java
+++ orcamento/src/main/java/br/com/hslife/orcamento/facade/IConta.java
@@ -1,45 +1,45 @@
 /***
-  
-  	Copyright (c) 2012 - 2021 Hércules S. S. José
 
-    Este arquivo é parte do programa Orçamento Doméstico.
-    
+	Copyright (c) 2012 - 2021 Hércules S. S. José
 
-    Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou
+	Este arquivo é parte do programa Orçamento Doméstico.
 
-    modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como
 
-    publicada pela Fundação do Software Livre (FSF); na versão 3.0 da
+	Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou
 
-    Licença.
-    
+	modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como
 
-    Este programa é distribuído na esperança que possa ser útil, mas SEM
+	publicada pela Fundação do Software Livre (FSF); na versão 3.0 da
 
-    NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer
-    
-    MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral Menor GNU
-    
-    em português para maiores detalhes.
-    
+	Licença.
 
-    Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob
-	
+
+	Este programa é distribuído na esperança que possa ser útil, mas SEM
+
+	NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer
+
+	MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral Menor
+
+	GNU em português para maiores detalhes.
+
+
+	Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob
+
 	o nome de "LICENSE" junto com este programa, se não, acesse o site do
-    
-    projeto no endereco https://github.com/herculeshssj/orcamento-maven ou 
-	
-	escreva para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth 
-	
-	Floor, Boston, MA  02110-1301, USA.
-    
 
-    Para mais informações sobre o programa Orçamento Doméstico e seu autor
-	
+	projeto no endereco https://github.com/herculeshssj/orcamento ou escreva
+
+	para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth 
+
+	Floor, Boston, MA  02110-1301, USA.
+
+
+	Para mais informações sobre o programa Orçamento Doméstico e seu autor
+
 	entre em contato pelo e-mail herculeshssj@outlook.com, ou ainda escreva
 
-    para Hércules S. S. José, Rua José dos Anjos, 160 - Bl. 3 Apto. 304 - 
-	
+	para Hércules S. S. José, Rua José dos Anjos, 160 - Bl. 3 Apto. 304 -
+
 	Jardim Alvorada - CEP: 26261-130 - Nova Iguaçu, RJ, Brasil.
 
 ***/
@@ -54,35 +54,38 @@ import br.com.hslife.orcamento.entity.Usuario;
 import br.com.hslife.orcamento.enumeration.TipoConta;
 
 public interface IConta extends ICRUDService<Conta> {
-	
-	public List<Conta> buscarTodos();
-	
-	public List<Conta> buscarPorDescricao(String descricao);
-	
-	public List<Conta> buscarTodosAtivos();
-	
-	public List<Conta> buscarTodosAtivosPorUsuario(Usuario usuario);
-	
-	public List<Conta> buscarPorUsuario(Usuario usuario);
-	
-	public List<Conta> buscarAtivosPorUsuario(Usuario usuario);
-	
-	public void ativarConta(Conta conta);
-	
-	public void desativarConta(Conta conta, String situacaoLancamentos);
-	
-	public List<Conta> buscarPorDescricaoEUsuario(String descricao, Usuario usuario);
-	
-	public List<Conta> buscarPorTipoContaEUsuario(TipoConta tipoConta, Usuario usuario);
-	
-	public List<Conta> buscarSomenteTipoCartaoPorUsuario(Usuario usuario);
-	
-	public List<Conta> buscarSomenteTipoCartaoAtivosPorUsuario(Usuario usuario);
-	
-	public Conta buscarPorCartaoCredito(CartaoCredito cartao);
-	
-	public List<Conta> buscarDescricaoOuTipoContaOuAtivoPorUsuario(String descricao, TipoConta[] tipoConta, Usuario usuario, Boolean ativo);
-	
-	public List<Conta> buscarTodosUsuarioPorFechamentoAutomatico(boolean fechaAutomaticamente);
-	
+
+	List<Conta> buscarTodos();
+
+	List<Conta> buscarPorDescricao(String descricao);
+
+	List<Conta> buscarTodosAtivos();
+
+	List<Conta> buscarTodosAtivosPorUsuario(Usuario usuario);
+
+	List<Conta> buscarPorUsuario(Usuario usuario);
+
+	List<Conta> buscarAtivosPorUsuario(Usuario usuario);
+
+	void ativarConta(Conta conta);
+
+	void desativarConta(Conta conta, String situacaoLancamentos);
+
+	List<Conta> buscarPorDescricaoEUsuario(String descricao, Usuario usuario);
+
+	List<Conta> buscarPorTipoContaEUsuario(TipoConta tipoConta, 
+			Usuario usuario);
+
+	List<Conta> buscarSomenteTipoCartaoPorUsuario(Usuario usuario);
+
+	List<Conta> buscarSomenteTipoCartaoAtivosPorUsuario(Usuario usuario);
+
+	Conta buscarPorCartaoCredito(CartaoCredito cartao);
+
+	List<Conta> buscarDescricaoOuTipoContaOuAtivoPorUsuario(String descricao, 
+			TipoConta[] tipoConta, Usuario usuario,	Boolean ativo);
+
+	List<Conta> buscarTodosUsuarioPorFechamentoAutomatico(
+			boolean fechaAutomaticamente);
+
 }
