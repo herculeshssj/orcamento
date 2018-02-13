31e04eb6fd4ca293261d2a6d81fd16dee1b20821
Hércules S. S. José <hercules_hssj@outlook.com>
2018-02-12 23:17:55 -0200

    Ajustes para diminiur os warnings no Checkstyle.

--- orcamento/src/test/java/br/com/hslife/orcamento/entity/ContaCompartilhadaTest.java
+++ orcamento/src/test/java/br/com/hslife/orcamento/entity/ContaCompartilhadaTest.java
@@ -29,7 +29,7 @@
 
 	projeto no endereco https://github.com/herculeshssj/orcamento ou escreva
 
-	para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth 
+	para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth
 
 	Floor, Boston, MA  02110-1301, USA.
 
@@ -42,8 +42,7 @@
 
 	Jardim Alvorada - CEP: 26261-130 - Nova Iguaçu, RJ, Brasil.
 
-***/
-package br.com.hslife.orcamento.entity;
+***/package br.com.hslife.orcamento.entity;
 
 import static org.junit.Assert.assertEquals;
 
