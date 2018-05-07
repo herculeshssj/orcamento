79b0fc67fa29ecc01bc5e797655a843661d9587e
Hércules S. S. José <hercules_hssj@outlook.com>
2018-05-06 21:04:26 -0300

    Implementação inicial das classes do módulo de Seguro.

---
+++ orcamento/src/main/java/br/com/hslife/orcamento/service/SeguroService.java
@@ -0,0 +1,65 @@
+/***
+
+Copyright (c) 2012 - 2021 Hércules S. S. José
+
+Este arquivo é parte do programa Orçamento Doméstico.
+
+
+Orçamento Doméstico é um software livre; você pode redistribui-lo e/ou
+
+modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como
+
+publicada pela Fundação do Software Livre (FSF); na versão 3.0 da
+
+Licença.
+
+
+Este programa é distribuído na esperança que possa ser útil, mas SEM
+
+NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer
+
+MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral Menor
+
+GNU em português para maiores detalhes.
+
+
+Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob
+
+o nome de "LICENSE" junto com este programa, se não, acesse o site do
+
+projeto no endereco https://github.com/herculeshssj/orcamento ou escreva
+
+para a Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth Floor,
+
+Boston, MA  02110-1301, USA.
+
+
+Para mais informações sobre o programa Orçamento Doméstico e seu autor
+
+entre em contato pelo e-mail herculeshssj@outlook.com, ou ainda escreva
+
+para Hércules S. S. José, Rua José dos Anjos, 160 - Bl. 3 Apto. 304 -
+
+Jardim Alvorada - CEP: 26261-130 - Nova Iguaçu, RJ, Brasil.
+
+***/
+package br.com.hslife.orcamento.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.hslife.orcamento.entity.Seguro;
+import br.com.hslife.orcamento.facade.ISeguro;
+import br.com.hslife.orcamento.repository.SeguroRepository;
+
+@Service
+public class SeguroService extends AbstractCRUDService<Seguro> implements ISeguro {
+
+	@Autowired
+	private SeguroRepository repository;
+	
+	public SeguroRepository getRepository() {
+		this.repository.setSessionFactory(this.sessionFactory);
+		return repository;
+	}
+}
